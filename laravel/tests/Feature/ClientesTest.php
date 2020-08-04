d5f6208d5f6c57e70a65b671fe70d603958a8f59
Pedro 'Coin' Duarte <coinbr@gmail.com>
2020-08-04 15:52:38 -0300

    Refactor Cliente teste to reutilize validDataArray

--- laravel/tests/Feature/ClientesTest.php
+++ laravel/tests/Feature/ClientesTest.php
@@ -13,37 +13,35 @@ class ClientesTest extends TestCase
 {
     use RefreshDatabase;
 
+
+    function data(){
+        return [
+            'nome' => 'Fulano',
+            'email' => 'fulano@cicrano.com',
+            'telefone' => '84988887777',
+            'endereco' => 'Avenida das Ruas, 85. Bairro: Barro Azul'
+        ];
+    }
+
     public function test_create_cliente(){
         $this->withoutExceptionHandling();
 
-        $clienteData = [
-            'nome' => 'Fulano',
-            'email' => 'fulano@cicrano.com',
-            'telefone' => '84988887777',
-            'endereco' => 'Avenida das Ruas, 85. Bairro: Barro Azul'
-        ];
-        $this->post('/api/clientes', $clienteData);
+        $this->post('/api/clientes', $this->data());
         $cliente = Cliente::first();
 
-        foreach($clienteData as $key => $value){
-            $this->assertEquals($clienteData[$key], $cliente[$key]);
+        foreach($this->data() as $key => $value){
+            $this->assertEquals($value, $cliente[$key]);
         }
     }
 
-    public function test_require_nome(){
-        $clienteData = [
-            'nome' => 'Fulano',
-            'email' => 'fulano@cicrano.com',
-            'telefone' => '84988887777',
-            'endereco' => 'Avenida das Ruas, 85. Bairro: Barro Azul'
-        ];
+    public function test_all_fields_are_required(){
+        $clienteData = $this->data();
 
         foreach ($clienteData as $k => $v){
             $incompleteData = $clienteData;
             unset($incompleteData[$k]);
             $this->post('/api/clientes', $incompleteData)->assertSessionHasErrors($k);
         }
-
         $this->assertCount(0, Cliente::all());
     }
 }
